ca8c14dc7fdc06a9fae93ca3773e55190f26e8f8
ojaswee <ojaswee@outlook.com>
2018-01-21 16:50:38 -0600

    clean up

--- Quick_Merge_Sort.py
+++ Quick_Merge_Sort.py
@@ -1,5 +1,6 @@
 # Quick sort is very effective sorting technique
 # most of the programs use this type of sorting when we call the sort function
+# We do all the sorting in same array and we DONOT create another array
 
 def quick_sort(Arr):
     partition_qS(Arr,0,len(Arr)-1)
@@ -28,19 +29,32 @@ def partition_qS(Arr,left, right):
 
 
 #----------------------Merge Sort -----------------
+# in merge sort we first divide the array into individual elements
+# compare this individual items and store then n temporary arrays
 def merge_Sort(Arr):
-    partition_mS(Arr, 0, len(Arr))
+    partition_mS(Arr, 0, len(Arr)-1)
 
 def partition_mS(Arr, left, right):
-    print Arr
+    mid = (left+right)/2
+    # print left, right
+    if (left) < mid:
+        partition_mS(Arr, left, mid)
+        partition_mS(Arr, mid+1, right)
+    if ( left== mid):
+        merge(Arr, left, right)
+        # print 'inside', left, right
 
+def merge(Arr, left, right):
+    count = right - left
+    temp = []
+    print Arr, left, right
 
 #---------------- Entry point for program exeution
 A = [3, 1, 9, 0, 12, 90, 45,65]
 print 'Unsorted Array:', A
-quick_sort(A)
-print 'Quick Sort:', A
+# quick_sort(A)
+# print 'Quick Sort:', A
 
 # A = [3, 1, 9, 0, 12, 90, 45]
 # print 'Merge Sort:'
-# merge_Sort(A)
+merge_Sort(A)
