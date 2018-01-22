e3e01db6984589521a2dfb41c46d8250ef5b217b
ojaswee <ojaswee@outlook.com>
2018-01-22 12:55:16 -0600

    merge sort

--- Quick_Merge_Sort.py
+++ Quick_Merge_Sort.py
@@ -11,15 +11,15 @@ def partition_qS(Arr,left, right):
     upper = right
     while (lower<upper):
         while pivot<Arr[upper] and lower < upper:
-            upper = upper-1
-        if (pivot>A[upper]):
+            upper -= 1
+        if (pivot>Arr[upper]):
             Arr[lower] = Arr[upper]
-            lower= lower+1
+            lower += 1
         while (pivot>= Arr[lower]) and lower < upper:
-            lower =lower+1
+            lower +=1
         if (pivot< Arr[lower]):
             Arr[upper]= Arr[lower]
-            upper = upper - 1
+            upper -=1
 
     Arr[lower] = pivot
     if (left<lower-1):
@@ -28,33 +28,49 @@ def partition_qS(Arr,left, right):
         partition_qS(Arr, upper+1,right)
 
 
-#----------------------Merge Sort -----------------
+# ----------------------Merge Sort -----------------
 # in merge sort we first divide the array into individual elements
 # compare this individual items and store then n temporary arrays
-def merge_Sort(Arr):
-    partition_mS(Arr, 0, len(Arr)-1)
 
-def partition_mS(Arr, left, right):
-    mid = (left+right)/2
-    # print left, right
-    if (left) < mid:
-        partition_mS(Arr, left, mid)
-        partition_mS(Arr, mid+1, right)
-    if ( left== mid):
-        merge(Arr, left, right)
-        # print 'inside', left, right
+def merge(a,b):
+    # create a new List
+    myList = []
+    countA, countB = len(a), len(b)
+    while countA> 0 and countB> 0:
+        if a[0] < b[0]:
+            myList.append(a[0])
+            a.remove(a[0])
+            countA -=1
+        else:
+            myList.append(b[0])
+            b.remove(b[0])
+            countB -=1
+    if countA == 0:
+        myList += b
+    else:
+        myList += a
+    return myList
 
-def merge(Arr, left, right):
-    count = right - left
-    temp = []
-    print Arr, left, right
+# Code for merge sort
 
-#---------------- Entry point for program exeution
-A = [3, 1, 9, 0, 12, 90, 45,65]
-print 'Unsorted Array:', A
-# quick_sort(A)
-# print 'Quick Sort:', A
+def mergesort(Arr):
+    # First partition array into individual elements
+    size = len(Arr)
+    if size <= 1:
+        return Arr
+    else:
+        mid = size/2
+        left = mergesort(Arr[:mid])
+        right = mergesort(Arr[mid:])
+    return merge(left, right)
 
-# A = [3, 1, 9, 0, 12, 90, 45]
+# ---------------- Entry point for program execution
+qS = [3, 1, 9, 0, 12, 90, 45, 65]
+print 'Unsorted Array:', qS
+quick_sort(qS)
+print 'Quick Sort:', qS
+
+mS = [3, 1, 9, 0, 12, 90, 45, 65]
 # print 'Merge Sort:'
-merge_Sort(A)
+mS = mergesort(mS)
+print 'Merge Sort:',mS
