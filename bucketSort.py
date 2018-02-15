410b0f4702494acb2f864463048410b25f622b87
ojaswee <ojaswee@outlook.com>
2018-02-15 11:45:37 -0600

    updated bucket

--- bucketSort.py
+++ bucketSort.py
@@ -1,33 +1,21 @@
-'''Bucket sort is similar to radix sort, the only difference is
-1) we distribute only once
-2) we need to create a container and
-3)within container we need to sort all the elemets
+'''Bucket sort as we put elements in bucket we need to sort it as well
+so first get an element then insert in the appropriate place in the bucket
 '''
 
 import time
 import random
 
-def sort_bucket(Arr,left, right):
-    lower = left
-    pivot = Arr[left]
-    upper = right
-    while (lower<upper):
-        while pivot<Arr[upper] and lower < upper:
-            upper -= 1
-        if (pivot>Arr[upper]):
-            Arr[lower] = Arr[upper]
-            lower += 1
-        while (pivot>= Arr[lower]) and lower < upper:
-            lower +=1
-        if (pivot< Arr[lower]):
-            Arr[upper]= Arr[lower]
-            upper -=1
-
-    Arr[lower] = pivot
-    if (left<lower-1):
-        sort_bucket(Arr,left,lower-1)
-    if (upper+1 < right):
-        sort_bucket(Arr, upper+1,right)
+def sort_bucket(A):  #insertion sort
+    size = len(A)
+    for i in range(1, size):
+        j = i-1
+        temp = A[i]
+        while j in range(0,size):
+            if (temp<A[j]):
+                A[j+1]=A[j]
+                j-=1
+            else: break
+        A[j+1] = temp
 
 #----------------------------------------------
 
@@ -36,42 +24,29 @@ def bucketSort(Arr, n=5):
     minVal = min(Arr)
     bucket = 5
     count = (maxVal-minVal)/bucket
-    count_elements = [0] * bucket    # counting number of elements in an bucket
     temp_Arr = []
 
     #put values in priority queue/dictionary
     for i in range(len(Arr)):
         if (Arr[i]<count):
             temp_Arr.append((0,Arr[i]))
-            count_elements[0] +=1
+            sort_bucket (temp_Arr)
         elif Arr[i] in range (count, 2*count):
             temp_Arr.append ((1, Arr[i]))
-            count_elements[1] += 1
+            sort_bucket(temp_Arr)
         elif Arr[i] in range (2*count, 3*count):
             temp_Arr.append ((2, Arr[i]))
-            count_elements[2] += 1
+            sort_bucket(temp_Arr)
         elif Arr[i] in range(3 * count, 4 * count):
             temp_Arr.append ((3, Arr[i]))
-            count_elements[3] += 1
+            sort_bucket(temp_Arr)
         else:
             temp_Arr.append ((4, Arr[i]))
-            count_elements[4] += 1
+            sort_bucket(temp_Arr)
 
+    #pop elements according to the bucket they are in
     temp_Arr= sorted(temp_Arr, key=lambda x: (x[0]))
     Arr = [i[1] for i in temp_Arr]
-    # print count_elements, Arr
-
-    # for i in range (len(Arr)):
-    start = 0
-    for j in range(bucket):
-        if j == 0 and count_elements[j]>0:
-            sort_bucket(Arr, 0, count_elements[j])
-            # print 0,count_elements[j]-1
-        elif j >0 and count_elements[j]>1:
-            end = start + count_elements[j] -1
-            # print start, end
-            sort_bucket(Arr,start, end)
-        start += count_elements[j]
     return Arr
 
 # bSort= [800,170, 45, 90, 24, 2,855, 900, 605, 66, 801]
@@ -79,13 +54,14 @@ def bucketSort(Arr, n=5):
 # bSort= bucketSort(bSort)
 # print bSort
 
-
+#
 start = time.time()
 for j in range(100): # loop 100 times
     bSort = []
-    for i in range(50000):  # elements in array
+    for i in range(10000):  # elements in array
         bSort.append(random.randrange(0,1000))
     bucketSort(bSort)
 end = time.time()
 t_time =(end - start)
+
 print 'Bucket time:',t_time
